01cc5fbf4d270f396e864d702f100d3270d528f9
Patrick Baselier <patrick.baselier@gmail.com>
2019-03-13 12:16:01 +0100

    Set expiresOn

--- app/components/warranty-form.js
+++ app/components/warranty-form.js
@@ -1,12 +1,32 @@
 import Component from "@ember/component";
+import { typeOf } from "@ember/utils";
+import { isPresent } from "@ember/utils";
 
 export default Component.extend({
+  dateFormat: "ddd, MMM Do, YYYY",
   actions: {
     submitChanges() {
       this.onSubmit();
     },
     cancelChanges() {
       this.onCancel();
+    }
+  },
+  didInsertElement() {
+    this.fixExpiresOnInPikaday();
+  },
+  fixExpiresOnInPikaday() {
+    //Wouldn't expect this to be necessary.
+    //See also: https://github.com/devotox/ember-paper-pikaday/issues/10
+    if (typeOf(this.warranty) === "instance") {
+      const expiresOn = this.warranty.get("expiresOn");
+
+      if (isPresent(expiresOn)) {
+        this.warranty.set(
+          "expiresOn",
+          moment(expiresOn).format(this.dateFormat)
+        );
+      }
     }
   }
 });
